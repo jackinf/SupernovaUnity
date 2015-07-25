c5a0245e32f33fcde38c2c8dc6eac408f3d5c0ab
Jevgeni Rumjantsev <zeka.rum@gmail.com>
2015-07-25 12:57:55 +0300

    imported assets: Standard Assets / ParticleSystems Standard Assets / Utility
    Asteroids now explode

--- Assets/Scripts/Asteroids/AsteroidInner.cs
+++ Assets/Scripts/Asteroids/AsteroidInner.cs
@@ -2,10 +2,13 @@
 
 public class AsteroidInner : MonoBehaviour
 {
+    public GameObject explosionTemplate;
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Bullet"))
         {
+            Instantiate(explosionTemplate, gameObject.transform.position, Quaternion.identity);
             Destroy(gameObject.transform.parent.gameObject);
         }
     }
